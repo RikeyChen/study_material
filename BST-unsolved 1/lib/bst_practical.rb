1143ae5d50bfa6ec0226a7ae54cbc66b6b6f1787
Rikey Chen <rikey.chen16@gmail.com>
2019-01-27 12:30:46 -0800

    Add kth_largest

--- BST-unsolved 1/lib/bst_practical.rb
+++ BST-unsolved 1/lib/bst_practical.rb
@@ -1,2 +1,8 @@
+require 'binary_search_tree'
+require 'bst_node'
+
 def kth_largest(tree_node, k)
+  tree = BinarySearchTree.new
+  result = tree.in_order_traversal(tree_node)
+  tree.find(result.reverse[k - 1], tree_node)
 end
