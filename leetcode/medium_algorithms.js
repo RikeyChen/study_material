b32956596558a5fed4be8ab3f500ec34cc6e710e
Rikey Chen <rikey.chen16@gmail.com>
2019-01-26 20:27:10 -0800

    Add rangeSumBST

--- leetcode/medium_algorithms.js
+++ leetcode/medium_algorithms.js
@@ -52,3 +52,31 @@ var constructMaximumBinaryTree = function (nums) {
 
   return root;
 };
+
+
+// Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R(inclusive).
+// The binary search tree is guaranteed to have unique values.
+
+//   Example 1:
+// Input: root = [10, 5, 15, 3, 7, null, 18], L = 7, R = 15
+// Output: 32
+
+var rangeSumBST = function (root, L, R) {
+  let sum = 0;
+
+  if (root.val >= L && root.val <= R) {
+    sum += root.val;
+  }
+
+  if (!root.left && !root.right) return sum;
+
+  if (root.left && root.val >= L) {
+    sum += rangeSumBST(root.left, L, R);
+  }
+
+  if (root.right && root.val <= R) {
+    sum += rangeSumBST(root.right, L, R);
+  }
+
+  return sum;
+};
