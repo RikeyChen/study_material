2ce049e28e5756a41cd0e347453b2ece8b4d124f
Rikey Chen <rikey.chen16@gmail.com>
2019-02-01 18:23:19 -0800

    Add singleNumber one line solution with no additional memory

--- leetcode/easy_algorithms.js
+++ leetcode/easy_algorithms.js
@@ -7,3 +7,13 @@ let singleNumber = function (nums) {
     if (numCounter[num] === 1) return num;
   }
 };
+
+// one line solution
+// explanation:
+// (1) a ^ b = b ^ a;
+// (2)(a ^ b) ^ c = a ^ (b ^ c);
+// (3) a ^ a = 0, a ^ 0 = a;
+// the ^ means XOR.
+
+//   a ^ b ^ b ^ c ^ c = a ^ 0 ^ 0 = a, so in this problem, the result is the element appears once.
+const singleNumber = (nums) => nums.reduce((accum, num) => accum ^ num, 0)
