3433bd1ae1049c1e6a1de099e55e3f525ea9e44f
Rikey Chen <rikey.chen16@gmail.com>
2019-02-02 23:18:49 -0800

    Add mergeTwoLists

--- leetcode/easy_algorithms.js
+++ leetcode/easy_algorithms.js
@@ -17,3 +17,39 @@ let singleNumber = function (nums) {
 
 //   a ^ b ^ b ^ c ^ c = a ^ 0 ^ 0 = a, so in this problem, the result is the element appears once.
 const singleNumber = (nums) => nums.reduce((accum, num) => accum ^ num, 0)
+
+var mergeTwoLists = function (l1, l2) {
+  if (!l1 && !l2) return null;
+  if (!l1) return l2;
+  if (!l2) return l1;
+  let head;
+
+  if (l1.val <= l2.val) {
+    head = l1;
+    l1 = l1.next;
+  } else {
+    head = l2;
+    l2 = l2.next;
+  }
+
+  let prevNode = head;
+
+  while (l1 && l2) {
+    if (l1 && l1.val <= l2.val) {
+      prevNode.next = l1;
+      l1 = l1.next;
+      prevNode = prevNode.next;
+    } else if (l2 && l2.val < l1.val) {
+      prevNode.next = l2;
+      l2 = l2.next;
+      prevNode = prevNode.next;
+    }
+  }
+  if (!l1) {
+    prevNode.next = l2;
+  } else if (!l2) {
+    prevNode.next = l1;
+  }
+
+  return head;
+};
