8c169551bd8a0895cf03b32124ab9c946db127ea
Rikey Chen <rikey.chen16@gmail.com>
2019-02-02 20:36:55 -0800

    Add longestPalindrome

--- leetcode/easy_algorithms.js
+++ leetcode/easy_algorithms.js
@@ -17,3 +17,27 @@ let singleNumber = function (nums) {
 
 //   a ^ b ^ b ^ c ^ c = a ^ 0 ^ 0 = a, so in this problem, the result is the element appears once.
 const singleNumber = (nums) => nums.reduce((accum, num) => accum ^ num, 0)
+
+// brute force way
+const longestPalindrome = (s) => {
+  if (s.length === 0) return ""
+  let longestPal = s[0];
+  for (idx1 = 0; idx1 < s.length - 1; idx1++) {
+    let substring = s[idx1];
+    for (idx2 = idx1 + 1; idx2 < s.length; idx2++) {
+      substring += s[idx2]
+      if (palindrome(substring) && substring.length > longestPal.length) {
+        longestPal = substring;
+      }
+    }
+  }
+  return longestPal;
+};
+
+const palindrome = (s) => {
+  for (i = 0, j = s.length - 1; i <= j; i++) {
+    if (s[i] !== s[j]) return false;
+    j--;
+  }
+  return true;
+}
