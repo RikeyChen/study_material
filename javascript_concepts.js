a6d86d257ec3f90a36b37dce0e98d91fabf90187
Rikey Chen <rikey.chen16@gmail.com>
2019-02-15 20:49:00 -0800

    Add throttle

--- javascript_concepts.js
+++ javascript_concepts.js
@@ -7,3 +7,25 @@ const debounce = (func, time) => {
     timeout = setTimeout(functionCall, time);
   };
 };
+
+const throttle = (callback, wait, immediate = false) => {
+  let timeout;
+  let initialCall = true;
+
+  return function (...args) {
+    const callNow = immediate && initialCall;
+    const next = () => {
+      callback.apply(this, args);
+      timeout = null;
+    };
+
+    if (callNow) {
+      initialCall = false;
+      next();
+    }
+
+    if (!timeout) {
+      timeout = setTimeout(next, wait);
+    }
+  };
+};
