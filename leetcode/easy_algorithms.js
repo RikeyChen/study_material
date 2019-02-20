516dc337192490a8b3eae2c23cc8a8c4ae672be0
Rikey Chen <rikey.chen16@gmail.com>
2019-02-19 22:50:48 -0800

    Add range sum query with caching solution

--- leetcode/easy_algorithms.js
+++ leetcode/easy_algorithms.js
@@ -173,3 +173,17 @@ NumArray.prototype.sumRange = function (i, j) {
   }
   return sum;
 };
+
+// Caching solution
+var NumArray = function (nums) {
+  this.sums = [];
+  let sum = 0;
+  for (i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    this.sums.push(sum);
+  }
+};
+
+NumArray.prototype.sumRange = function (i, j) {
+  return this.sums[j] - (i > 0 ? this.sums[i - 1] : 0)
+};
