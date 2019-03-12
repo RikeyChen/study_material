421048ce47e24eb0493c1aa7120f9d974319990c
Rikey Chen <rikey.chen16@gmail.com>
2019-03-11 19:56:06 -0700

    Add refactored second highest salary

--- leetcode/database.sql
+++ leetcode/database.sql
@@ -12,3 +12,12 @@ SELECT
     ORDER BY Salary DESC
     LIMIT 1
     OFFSET 1) AS SecondHighestSalary
+
+-- Second attempt
+SELECT IFNULL(
+    (SELECT DISTINCT Salary
+    FROM Employee
+    ORDER BY Salary DESC
+    LIMIT 1
+    OFFSET 1),
+    NULL) AS SecondHighestSalary
