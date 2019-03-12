b6ed709862567599af6ff3998f783ad9ae19b401
Rikey Chen <rikey.chen16@gmail.com>
2019-03-11 19:54:07 -0700

    Add second highest salary SQL problem

--- leetcode/database.sql
+++ leetcode/database.sql
@@ -3,3 +3,12 @@ SELECT FirstName, LastName, City, State
 FROM Person
 LEFT JOIN Address
 ON Person.PersonId = Address.PersonId
+
+-- Second Highest Salary
+-- First attempt
+SELECT
+    (SELECT DISTINCT Salary
+    FROM Employee
+    ORDER BY Salary DESC
+    LIMIT 1
+    OFFSET 1) AS SecondHighestSalary
