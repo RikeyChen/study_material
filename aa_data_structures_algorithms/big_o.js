d2fc0c185089bbd048c4a3199743ddb64594f601
Rikey Chen <rikey.chen16@gmail.com>
2019-03-13 14:15:13 -0700

    Add myMin phase I

--- aa_data_structures_algorithms/big_o.js
+++ aa_data_structures_algorithms/big_o.js
@@ -22,3 +22,28 @@ const findMissingNumber = (arr1, arr2) => {
 // console.log(findMissingNumber([1, 1, 1, 1], [1, 1, 1])); // = 1
 
 // console.log(findMissingNumber([3, 5, 4, 8, 7, 9], [7, 4, 3, 5, 9])); // = 8
+
+
+// myMin
+// Given a list of integers find the smallest number in the list.
+
+// list = [0, 3, 5, 4, -5, 10, 1, 90]
+// my_min(list) # => -5
+
+// Phase I
+// First, write a function that compares each element to every other element of the list.Return the element if all other elements in the array are larger.
+
+// What is the time complexity for this function?
+// O(n^2) - for every element of the input, you need to compare it to every
+// other element in the input
+const myMin = (list) => {
+  for (let i = 0; i < list.length; i++) {
+    let smallest = true;
+    for (let j = 0; j < list.length; j++) {
+      if (list[j] < list[i]) smallest = false;
+    }
+    if (smallest) return list[i];
+  }
+};
+
+// console.log(myMin([0, 3, 5, 4, -5, 10, 1, 90]));
