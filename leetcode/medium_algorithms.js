54e080c774c60c6662b489d9f99dd0f81fd97b9f
Rikey Chen <rikey.chen16@gmail.com>
2019-03-16 22:16:33 -0700

    Add coinChange2

--- leetcode/medium_algorithms.js
+++ leetcode/medium_algorithms.js
@@ -515,3 +515,20 @@ const letterCombinations = (digits, current = '', res = []) => {
 
   return res;
 };
+
+// coin change 2
+const change = (amount, coins, combinations = {}) => {
+  const key = `${amount  }-${  coins}`;
+  if (key in combinations) return combinations[key];
+  if (amount === 0) return 1;
+
+  const currentCoin = coins[coins.length - 1];
+  let comboAmount = 0;
+
+  for (let qty = 0; qty * currentCoin <= amount; qty++) {
+    comboAmount += change(amount - qty * currentCoin, coins.slice(0, -1), combinations);
+  }
+
+  combinations[key] = comboAmount;
+  return combinations[key];
+};
